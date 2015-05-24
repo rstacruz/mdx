d5f357a17b6944cf427e116a160a98ff99a5a1e8
Rico Sta. Cruz <hi@ricostacruz.com>
2015-05-24 18:56:01 +0800

    Update tests

---
+++ test/parsing_test.js
@@ -0,0 +1,88 @@
+parsingTest(
+'basic parsing.js',
+`/*
+ * Description here.
+ */
+
+function hello() {
+}`,
+function() {
+  it('has locations', function () {
+    expect(this.out[0].location.doc).eql({ start: 1, end: 3 });
+   expect(this.out[0].location.code).eql({ start: 5 });
+ });
+   
+  it('has prelude', function () {
+    expect(this.out[0].prelude).eql("function hello() {");
+  });
+
+  it('has raw', function () {
+    expect(this.out[0].raw).eql("Description here.\n");
+  });
+});
+
+parsingTest(
+'multiple blocks.js',
+`/*
+ * Description here.
+ */
+
+ function hello() {
+ }
+
+/*
+ * Other description here.
+ */
+
+ function world() {
+ }`,
+function () {
+  it('has locations [0]', function () {
+    expect(this.out[0].location.doc).eql({ start: 1, end: 3 });
+    expect(this.out[0].location.code).eql({ start: 5 });
+  });
+
+  it('has locations [1]', function () {
+    expect(this.out[1].location.doc).eql({ start: 8, end: 10 });
+    expect(this.out[1].location.code).eql({ start: 12 });
+  });
+    
+  it('has prelude[0]', function () {
+    expect(this.out[0].prelude).eql("function hello() {");
+  });
+
+  it('has prelude[1]', function () {
+    expect(this.out[1].prelude).eql("function world() {");
+  });
+
+  it('has raw', function () {
+    expect(this.out[0].raw).eql("Description here.\n");
+  });
+});
+
+parsingTest(
+'stray closing tags.js',
+`/*
+ * Description here.
+ */
+
+ function hello() {
+       */ }`,
+function () {
+  it('has locations', function () {
+    expect(this.out[0].location.doc).eql({ start: 1, end: 3 });
+    expect(this.out[0].location.code).eql({ start: 5 });
+  });
+});
+
+
+parsingTest('explicit flag.js',
+`/**
+ * Description here.
+ */
+function hello() {}`,
+function () {
+  it('sets the explicit flag', function () {
+    expect(this.out[0].explicit).eql(true);
+  });
+});
--- test/setup.js
+++ test/setup.js
@@ -1,1 +1,10 @@
 global.expect = require('chai').expect;
+
+/* Describe a parsing test case */
+global.parsingTest = function (name, src, fn) {
+  const push = require('./helpers/push');
+  describe(name, function () {
+    push(name, src);
+    fn();
+  });
+};
